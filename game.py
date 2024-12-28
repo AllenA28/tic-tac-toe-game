24cf89001dbc2c93ae183d0920f4796e715eaa62
Allen Thomas <63938772+AllenA28@users.noreply.github.com>
2024-12-28 13:19:24 -0600

    added comments

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-# Example file showing a circle moving on screen
+
 import pygame
 
 # pygame setup
@@ -13,7 +13,7 @@ font = pygame.font.SysFont('Helvetica', 720//3, bold=True, italic=False)
 instrfont = pygame.font.SysFont('Helvetica', 50, bold=True, italic=False)
 winner = ['tie', 'player 1', 'player 2']
 
-
+#honestly this one was probably not needed, just for fun
 class Board:
     def __init__(self, color):
         self.color = color
@@ -24,7 +24,7 @@ class Board:
                 self.tiles[i].append(Tile(TILE_SIZE, (j, i)))
 
 
-
+#each tile is basically it's own object with properties like coords, length, and the marker it shows
 class Tile:
     def __init__(self, length, coords):
         self.length = length
@@ -38,29 +38,44 @@ class Tile:
         pygame.draw.rect(screen, 'black', self.rect, round(self.length / 30))
         screen.blit(font.render(self.marker, True, 'black'), (self.xval+25, self.yval+25))
 
+
 def get_clicked_tile(mouse_pos, board):
+    '''
+    this returns the tile that was clicked, requires mouse position and the board
+    '''
     for row in board.tiles:
         for tile in row:
             if tile.rect.collidepoint(mouse_pos):
                 return tile
     return None
 
+
 def check_winner():
+    '''
+like the method name says, it checks for a winner, it scanns the board for win conditions
+returns a boolean determining if someone won the game, changes the global variables for 
+message, and the start and endpoint of the winning line
+    '''
     global message,spoint,epoint
 
     for item in 'XO':
+        #check for three in a row
         for row in range(3):
             if all(board.tiles[row][col].marker == item for col in range(3)):
                 spoint = (0,row*TILE_SIZE+TILE_SIZE/2)
                 epoint = (TILE_SIZE*3-(TILE_SIZE/30),row*TILE_SIZE+ TILE_SIZE/2)
                 message = f'Player {TILE_MARKERS.index(item)} wins by Row!'
                 return True
+
+        #check for three in a column
         for col in range(3):
             if all(board.tiles[row][col].marker == item for row in range(3)):
                 message = f'Player {TILE_MARKERS.index(item)} wins by Column!'
                 spoint = (col*TILE_SIZE+TILE_SIZE/2,0)
                 epoint = (col*TILE_SIZE+ TILE_SIZE/2,TILE_SIZE*3-(TILE_SIZE/30))
                 return True
+
+        #Check the diagonals
         if (board.tiles[0][0].marker == board.tiles[1][1].marker == board.tiles[2][2].marker == item):
             message = f'Player {TILE_MARKERS.index(item)} wins by Diagonal!'
             spoint = board.tiles[0][0].rect.topleft
@@ -71,6 +86,7 @@ def check_winner():
             spoint = board.tiles[0][2].rect.topright
             epoint = board.tiles[2][0].rect.bottomleft
             return True
+            
     return False
 
 board = Board('blue')
@@ -80,13 +96,13 @@ count=0
 game_over = False
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+################################### inputs #########################
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
+                #this allows for restarting the game
                 for row in range(3):
                     for col in range(3):
                         board.tiles[row][col].marker = TILE_MARKERS[0]
@@ -106,21 +122,20 @@ while running:
 
 
 
-    # fill the screen with a color to wipe away anything from last frame
+    ################################# Display stuff ##############################
     screen.fill('black')
 
-
-
-    
     draw = False
     if count == 9:
         message = 'It\'s a draw!'
         draw = True
         game_over = True
-    # flip() the display to put your work on screen
+    
     for row in board.tiles:
         for tile in row:
             tile.render(screen)
+
+    #things for game over
     if game_over:
         screen.blit(instrfont.render(message, True, 'white'), (14, 720))
         if not draw:
@@ -128,10 +143,6 @@ while running:
     else:
         screen.blit(instrfont.render(f'Player {player}, your move', True, 'white'), (14, 720))
     
-    
-    
-    
-
 
 
     pygame.display.flip()
